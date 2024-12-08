b3b272a45a0cae6f60ca33fd619b66be202ded1a
Matt Kavalauskas <matt@kavalauskas.net>
2024-12-07 19:53:14 -0600

    more cleanup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SamplesAuto.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.util.ElapsedTime.Resolution.SECONDS;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Config
+@Autonomous(group = "a")
+
+
+public class SamplesAuto extends LinearOpMode
+{
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        //Pose2d purplePixelDropoff = new Pose2d(0,0,0);
+        Pose2d initPose = new Pose2d(0,0,Math.toRadians(90));
+        Pose2d toSubmursible = new Pose2d(0,0,0);
+        Pose2d left = new Pose2d(0,0,0);
+        Pose2d dropoff = new Pose2d(0,0,0);
+        Pose2d mid = new Pose2d(0,0,0);
+        Pose2d right = new Pose2d(0,0,0);
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
+        ExtraOpModeFunctions extras = new ExtraOpModeFunctions(hardwareMap, this);
+
+        //sleep(500);
+        //extras.initElevator();
+
+        telemetry.addLine("Initialized");
+        telemetry.addData("x", drive.pose.position.x);
+        telemetry.addData("y", drive.pose.position.y);
+        telemetry.addData("heading", drive.pose.heading);
+        telemetry.update();
+
+
+        while (!isStopRequested() && !opModeIsActive())
+        {
+            sleep(10);
+        }
+
+        toSubmursible = new Pose2d(-15, 10, Math.toRadians(45));
+        //left = new Pose2d(35, 31, Math.toRadians(0));
+        //dropoff = new Pose2d(35, 31, Math.toRadians(0));
+
+        Action DriveToBasketPole = drive.actionBuilder(drive.pose)
+                .strafeToLinearHeading(toSubmursible.position, toSubmursible.heading, new TranslationalVelConstraint(15.0))
+                .build();
+
+        Actions.runBlocking(new ParallelAction(
+                DriveToBasketPole,
+                new SequentialAction(
+                        new SleepAction(0),
+                        new InstantAction(() -> extras.armVertical()),
+                        new InstantAction(() -> extras.elevatorHighBasket()),
+                        new SleepAction(5),
+                        new InstantAction(() -> extras.sampleDump())
+                )
+        ));
+
+        sleep(5000);
+        //Drop off
+
+        //Mid sample
+
+        //Drop off
+
+        //Right sample
+
+        //Drop off
+
+        //Grab Specimen 1
+
+        //Drop off specimen 1
+
+        //Grab Specimen 2
+
+        //Drop off specimen 2
+
+        //Grab Specimen 3
+
+        //Drop off specimen 3
+
+        //Grab Specimen 4
+
+        //Drop off specimen 4
+
+        extras.saveAutoStartRotation(drive.odo.getHeading());
+
+
+    }
+
+
+    public void safeWaitSeconds(double time)
+    {
+        ElapsedTime timer = new ElapsedTime(SECONDS);
+        timer.reset();
+        while (!isStopRequested() && timer.time() < time) {
+        }
+    }
+
+
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimensAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimensAuto.java
@@ -12,7 +12,6 @@ import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -20,7 +19,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(group = "a")
 
 
-public class RRAuto extends LinearOpMode
+public class SpecimensAuto extends LinearOpMode
 {
 
     @Override

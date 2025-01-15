e900cf7a96966fc1b83612ec7d4ff0a0ceba7c63
rahnjac <rahnjac@kmsd.edu>
2025-01-14 20:11:53 -0600

    Pinpoint auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimensAutoPush3PinPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpecimensAutoPush3PinPoint.java
@@ -31,16 +31,17 @@ public class SpecimensAutoPush3PinPoint extends LinearOpMode
     {
         double initialRotation = 270;
         Pose2d initPose = new Pose2d(0,0,Math.toRadians(initialRotation));
-        Pose2d toSubmursible = new Pose2d(-12,30,Math.toRadians(270));
-        Pose2d backUpFromSubmursible = new Pose2d(20,26,Math.toRadians(270));
-        Pose2d lineUpForSweep = new Pose2d(30,50,Math.toRadians(270));
-        Pose2d slideOver1 = new Pose2d(32,53,Math.toRadians(270));
-        Pose2d sweep = new Pose2d(32,14,Math.toRadians(270));
-        Pose2d backTo1 = new Pose2d(32,50,Math.toRadians(270));
-        Pose2d slideOver2 = new Pose2d(41,50,Math.toRadians(270));
-        Pose2d sweep2 = new Pose2d(41,14,Math.toRadians(270));
-        Pose2d backTo2 = new Pose2d(41,50,Math.toRadians(270));
-        Pose2d slideOver3 = new Pose2d(48,50,Math.toRadians(270));
+        Pose2d toSubmursible = new Pose2d(-12,29,Math.toRadians(270));
+        Pose2d backUpFromSubmursible = new Pose2d(18,26,Math.toRadians(270));
+        //Pose2d lineUpForSweep = new Pose2d(30,50,Math.toRadians(270));
+        Pose2d forward1 = new Pose2d(18,38,Math.toRadians(270));
+        Pose2d slideOver1 = new Pose2d(30,44,Math.toRadians(270));
+        Pose2d sweep = new Pose2d(30,14,Math.toRadians(270));
+        Pose2d backTo1 = new Pose2d(30,38,Math.toRadians(270));
+        Pose2d slideOver2 = new Pose2d(43,44,Math.toRadians(270));
+        Pose2d sweep2 = new Pose2d(43,14,Math.toRadians(270));
+        Pose2d backTo2 = new Pose2d(43,38,Math.toRadians(270));
+        Pose2d slideOver3 = new Pose2d(48,44,Math.toRadians(270));
         Pose2d sweep3 = new Pose2d(48,14,Math.toRadians(270));
         Pose2d lineUpForWallSlide = new Pose2d(10,-3,Math.toRadians(180));
         Pose2d wallSlide = new Pose2d(51,-3,Math.toRadians(180));
@@ -56,6 +57,9 @@ public class SpecimensAutoPush3PinPoint extends LinearOpMode
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, initPose);
         ExtraOpModeFunctions extras = new ExtraOpModeFunctions(hardwareMap, this);
+
+        drive.PARAMS.headingGain = 4;
+        drive.PARAMS.lateralGain = 8;
 
         extras.initArm();
         extras.tailUp();
@@ -93,18 +97,17 @@ public class SpecimensAutoPush3PinPoint extends LinearOpMode
 
         Actions.runBlocking(
                 drive.actionBuilder(drive.pose)
-                        .strafeToLinearHeading(backUpFromSubmursible.position, backUpFromSubmursible.heading, new TranslationalVelConstraint(50.0))
-                        //.strafeToLinearHeading(lineUpForSweep.position, lineUpForSweep.heading, new TranslationalVelConstraint(50.0))
-                        .strafeToLinearHeading(slideOver1.position, slideOver1.heading, new TranslationalVelConstraint(50.0))
-                        .strafeToLinearHeading(sweep.position, sweep.heading, new TranslationalVelConstraint(50.0))
+                        .splineToLinearHeading(backUpFromSubmursible, 90, new TranslationalVelConstraint(50.0))
+                        .splineToLinearHeading(forward1, 90, new TranslationalVelConstraint(50.0))
+                        .splineToLinearHeading(slideOver1, 180, new TranslationalVelConstraint(50.0))
+                        .splineToLinearHeading(sweep, sweep.heading, new TranslationalVelConstraint(50.0))
                         .strafeToLinearHeading(backTo1.position, backTo1.heading, new TranslationalVelConstraint(50.0))
-                        .strafeToLinearHeading(slideOver2.position, slideOver2.heading, new TranslationalVelConstraint(50.0))
+                        .splineToLinearHeading(slideOver2, slideOver2.heading, new TranslationalVelConstraint(50.0))
                         .strafeToLinearHeading(sweep2.position, sweep2.heading, new TranslationalVelConstraint(50.0))
                         .strafeToLinearHeading(backTo2.position, backTo2.heading, new TranslationalVelConstraint(50.0))
-                        .strafeToLinearHeading(slideOver3.position, slideOver3.heading, new TranslationalVelConstraint(50.0))
+                        .splineToLinearHeading(slideOver3, slideOver3.heading, new TranslationalVelConstraint(50.0))
                         .strafeToLinearHeading(sweep3.position, sweep3.heading, new TranslationalVelConstraint(50.0))
                         .build());
-
 
 
         Actions.runBlocking(
